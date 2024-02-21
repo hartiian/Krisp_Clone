d74086ba46470afefc4e907805816965c3e20a8e
Arpine Harutyunyan <159726808+hartiian@users.noreply.github.com>
2024-02-21 23:46:42 +0400

    task6.cpp

--- task6.cpp
+++ task6.cpp
@@ -4,7 +4,7 @@ int main()
 {
     int num = 10;
     int* p = &num;
-	int& ref = num;
+    int& ref = num;
     std::cout << "Original number is: " << num << std::endl;
     *p = 20;
     std::cout << "After pointed: " << num << std::endl;
