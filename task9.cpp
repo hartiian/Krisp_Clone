218aeb51b0536f31566cd2c804c87d8c058420fc
Arpine Harutyunyan <159726808+hartiian@users.noreply.github.com>
2024-02-22 02:10:44 +0400

    task9.cpp

--- task9.cpp
+++ task9.cpp
@@ -39,17 +39,17 @@ void selection(int* arr, int size)
 {
 	for (int i = 0; i < size - 1; i++) 
 	{
-        	int min_index = i;
-        	for (int j = i + 1; j < size; j++) 
-		{
-            		if (arr[j] < arr[min_index]) 
-	    		{
-                		min_index = j;
-            		}
-        	}
-        	int temp = arr[i];
-        	arr[i] = arr[min_index];
-        	arr[min_index] = temp;
+            int min_index = i;
+            for (int j = i + 1; j < size; j++) 
+	    {
+		if (arr[j] < arr[min_index]) 
+  		{
+		    min_index = j;
+            	}
+	    }
+            int temp = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index] = temp;
     	}
 }
 
