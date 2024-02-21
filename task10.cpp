ab73a214f48b67246007af06240885aa9e989d44
Arpine Harutyunyan <159726808+hartiian@users.noreply.github.com>
2024-02-22 02:29:18 +0400

    Add files via upload

---
+++ task10.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() 
+{
+    std::fstream output("example.txt");
+    if (!output.is_open()) 
+	{
+        std::cerr << "Error!\n";
+    }
+    output << "Arpine\n";
+    output << "Anahit\n";
+    output << "Astghik\n";
+    output << "Emma\n";
+    output.close();
+
+    std::fstream input("example.txt");
+    if (!input.is_open()) 
+	{
+        std::cerr << "Error!\n";
+    }
+    std::string name;
+    std::cout << "Contents of the file:\n";
+    while (std::getline(input, name)) 
+	{
+        std::cout << name << std::endl;
+    }
+    input.close();
+    return 0; 
+}
